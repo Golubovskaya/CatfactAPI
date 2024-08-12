1940dc6383f35532e38285e839bdc95b3742027d
Nataliya Golubovskaya <nattwork@gmail.com>
2024-08-12 13:30:53 +0300

    Code fixed

--- mainTest.py
+++ mainTest.py
@@ -2,38 +2,29 @@ import requests
 
 base_url = "https://catfact.ninja"
 
+# Получение списка пород
 print("Породы")
-endpoint = "/breeds"
-query_params = {'limit':'10'}
+breeds_endpoint = "/breeds"
+breeds_query_params = {'limit': '10'}
+breeds_response = requests.get(base_url + breeds_endpoint, params=breeds_query_params)
+print(breeds_response.status_code)
+print(breeds_response.text)
 
-response = requests.get(base_url+endpoint, params=query_params)
-print(response.status_code)
-print(response.text)
-
-
-
-response = requests.get(base_url+endpoint, params=query_params)
-print(response.status_code)
-print(response.text)
-
+# Получение случайного факта
 print("Случайный факт")
-endpoint = "/fact"
-query_params = {
-    'max_lenght':'100'
-}
+fact_endpoint = "/fact"
+fact_query_params = {'max_length': '100'}
+fact_response = requests.get(base_url + fact_endpoint, params=fact_query_params)
+print(fact_response.status_code)
+print(fact_response.text)
 
-response = requests.get(base_url+endpoint, params=query_params)
-print(response.status_code)
-print(response.text)
-
-
+# Получение списка фактов
 print("Список фактов")
-endpoint = "/facts"
-query_params = {
-    'limit':'3',
-    'max_lenght':'90'
+facts_endpoint = "/facts"
+facts_query_params = {
+    'limit': '3',
+    'max_length': '90'
 }
-
-response = requests.get(base_url+endpoint, params=query_params)
-print(response.status_code)
-print(response.text)
+facts_response = requests.get(base_url + facts_endpoint, params=facts_query_params)
+print(facts_response.status_code)
+print(facts_response.text)
